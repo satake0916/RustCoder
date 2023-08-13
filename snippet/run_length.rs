0f1b5ac283dff2c018dcc6388889151d5d9a7596
satake0916 <stk_halfmoon@icloud.com>
2023-08-13 11:50:13 +0900

    make Run Length snippet

---
+++ snippet/run_length.rs
@@ -0,0 +1,13 @@
+#[snippet("runlength")]
+// ---------- begin Run Length Encoding ----------
+fn run_length_encoding<T: Eq>(a: Vec<T>) -> Vec<(T, usize)> {
+    let mut a = a.into_iter().map(|a| (a, 1)).collect::<Vec<_>>();
+    a.dedup_by(|a, b| {
+        a.0 == b.0 && {
+            b.1 += a.1;
+            true
+        }
+    });
+    a
+}
+// ---------- end Run Length Encoding ----------
